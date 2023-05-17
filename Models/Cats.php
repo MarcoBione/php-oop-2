2ea9b7549cb4c7b7402ebfc1350301ceefcd5caa
MarcoBione <marcobione.work@gmail.com>
2023-05-17 16:35:29 +0200

    Add Genre + add genre includes

---
+++ Models/Cats.php
@@ -0,0 +1,16 @@
+<?php
+include __DIR__.'/Genre.php';
+class Cats{
+
+    public
+    $name,$type,$material,$price;
+
+    function __construct(string $name='', string $type='', string $material='', string $price='', Genre $genre){
+        $this->name = $name;
+        $this->type = $type;
+        $this->material = $material;
+        $this->price = $price;
+        $this->genre = $genre;
+    }
+
+}
