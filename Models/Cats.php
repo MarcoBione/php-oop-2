da46a076cb88019fd48c09132951fc1a66b47813
MarcoBione <marcobione.work@gmail.com>
2023-05-17 16:53:13 +0200

    Add some details @ Dogs @ Cats + items @ db

--- Models/Cats.php
+++ Models/Cats.php
@@ -3,14 +3,15 @@ include __DIR__.'/Genre.php';
 class Cats{
 
     public
-    $name,$type,$material,$price,$genre;
+    $name,$type,$material,$price,$genre, $img;
 
-    function __construct(string $name='', string $type='', string $material='', string $price='', Genre $genre){
+    function __construct(string $name ='', string $type='', string $material='', string $price='', Genre $genre, string $img){
         $this->name = $name;
         $this->type = $type;
         $this->material = $material;
         $this->price = $price;
         $this->genre = $genre;
+        $this->img = $img;
     }
 
 }
